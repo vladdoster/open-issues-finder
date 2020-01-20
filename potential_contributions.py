170ce1206c601b2244c0d5fd11df923db673024e
Vlad Doster <mvdoster@gmail.com>
2020-01-19 21:03:30 -0500

    fix(Added threaded concurrency): Raymond Hettinger making me a better programmer with concurrency and cleaned up choice logic by reducing mental registers

--- potential_contributions.py
+++ potential_contributions.py
@@ -1,45 +1,56 @@
 import configparser
-import random
+import pprint
 import smtplib
+from multiprocessing.pool import ThreadPool as Pool
+from random import *
 from time import sleep
 
-import httpx as httpx
+import httpx
+from httpx import get
+
+pp = pprint.PrettyPrinter(indent=1)
 
 
 def main():
     cfg = configparser.ConfigParser(allow_no_value=True)
     cfg.read('config.ini')
     interests, labels = get_section(cfg, section='Interests'), get_section(cfg, section='Labels')
-    links = get_html_links(get_github_projects(cfg, interests=interests, labels=labels))
+    links = multiprocess_links(gen_gh_interests_urls(cfg=cfg, interests=interests, labels=labels))
+    pp.pprint(links)
     send_email(cfg, links)
 
 
-def get_github_projects(cfg, interests, labels):
+def gen_gh_interests_urls(cfg, interests, labels):
     projects_limit = cfg['User']['projects_limit']
     links = []
     for x in range(int(projects_limit)):
-        interest, label = random.choice(interests), random.choice(labels)
+        interest, label = choice(interests), choice(labels)
         links.append(
             f"https://api.github.com/search/issues?q={interest}+label:{label}+state:open&sort=created&order=desc")
     return links
 
 
-def get_html_links(links):
-    html_links = []
-    for link in links:
-        sleep(1)
-        try:
-            r = httpx.get(link)
-            r.raise_for_status()
-            r = r.json()
-            # TODO: Implement pagination using modulo math to get older issues
-            html_link = r.get('items')[random.randrange(0, len(r.get('items')), 1)]
-            html_links.append(html_link.get('html_url'))
-        except httpx.exceptions.HTTPError as e:
-            print(f"Requesting from Github too often\n{e}")
-        except Exception as e:
-            print(f"Something unexpected occurred...\n{e}")
-    return html_links
+def multiprocess_links(urls):
+    results = []
+    pool = Pool(10)
+    for result in pool.imap_unordered(get_gh_link, urls):
+        if result is not None:
+            results.append(result)
+    return results
+
+
+def get_gh_link(url):
+    sleep(randint(1, 2))
+    try:
+        r = get(url)
+        r.raise_for_status()
+        r = r.json()
+        gh_link = choice(r.get('items')).get('html_url')
+        return gh_link
+    except httpx.exceptions.HTTPError as e:
+        print(f"Requesting from Github too often")
+    except Exception as e:
+        print(f"Something unexpected occurred...\n{e}")
 
 
 def get_section(cfg, section):
@@ -53,7 +64,7 @@ def get_section(cfg, section):
     except OSError:
         print("Error: No config.ini found.")
         exit(1)
-    
+
     # Make sure user provides >= 1 interest
     # I learned that you should not do len(entries)==0, but just not. Python will return false if len == 0
     if not entries:
@@ -66,15 +77,14 @@ def send_email(cfg, links):
     email, name, password = cfg["User"]["email"], cfg["User"]["name"], cfg["User"]["Email_Pass"]
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     # If using Gmail, generate an app password @ https://myaccount.google.com/apppasswords
-    server.login(email, password)
+    server.login(user=email, password=password)
     links = '\n'.join(links)
     server.sendmail(
-        email,
-        email,
-        f"""Subject: Potential Contributor\n\nHi {name},\nWe found the following projects you might be interested in contributing too.\n\n{links}""")
+        from_addr=email,
+        to_addrs=email,
+        msg=f"""Subject: Potential Contributor\n\nHi {name},\nWe found the following projects you might be interested in contributing too.\n\n{links}""")
     server.quit()
     print("Successfully sent Github repos to your email!")
-
 
 
 if __name__ == '__main__':
