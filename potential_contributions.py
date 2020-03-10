ab75922a64dc52a03e1ef608a4a573dceef91194
Vlad Doster <mvdoster@gmail.com>
2020-03-09 20:34:12 -0400

    moved to list comprehension

--- potential_contributions.py
+++ potential_contributions.py
@@ -1,6 +1,7 @@
 import configparser
 import pprint
 import smtplib
+import sys
 from multiprocessing.pool import ThreadPool as Pool
 from random import *
 from time import sleep
@@ -16,12 +17,15 @@ def main():
     cfg.read('config.ini')
     interests, labels = get_section(cfg, section='Interests'), get_section(cfg, section='Labels')
     links = multiprocess_links(gen_gh_interests_urls(cfg=cfg, interests=interests, labels=labels))
-    pp.pprint(links)
+    # pp.pprint(links)
     send_email(cfg, links)
 
 
 def gen_gh_interests_urls(cfg, interests, labels):
     projects_limit = cfg['User']['projects_limit']
+    if projects_limit is None:
+        print("Set a project limit in config")
+        sys.exit(1)
     links = []
     for x in range(int(projects_limit)):
         interest, label = choice(interests), choice(labels)
@@ -31,11 +35,8 @@ def gen_gh_interests_urls(cfg, interests, labels):
 
 
 def multiprocess_links(urls):
-    results = []
     pool = Pool(10)
-    for result in pool.imap_unordered(get_gh_link, urls):
-        if result is not None:
-            results.append(result)
+    results = [x for x in pool.imap_unordered(get_gh_link, urls) if x is not None]
     return results
 
 
@@ -47,10 +48,10 @@ def get_gh_link(url):
         r = r.json()
         gh_link = choice(r.get('items')).get('html_url')
         return gh_link
-    except httpx.exceptions.HTTPError as e:
+    except httpx.exceptions.HTTPError:
         print(f"Requesting from Github too often")
-    except Exception as e:
-        print(f"Something unexpected occurred...\n{e}")
+    except Exception as error:
+        print(f"Something unexpected occurred...\n{error}")
 
 
 def get_section(cfg, section):
@@ -60,21 +61,24 @@ def get_section(cfg, section):
         entries = cfg.items(section)
     except configparser.NoSectionError:
         print(f"Error: Add a [{section}] section to your config.ini")
-        exit(1)
+        sys.exit(1)
     except OSError:
         print("Error: No config.ini found.")
-        exit(1)
+        sys.exit(1)
 
     # Make sure user provides >= 1 interest
     # I learned that you should not do len(entries)==0, but just not. Python will return false if len == 0
     if not entries:
         print(f"Add at least one {section[:-1]} in your config.ini")
-        exit(1)
+        sys.exit(1)
     return [str(x[0]).lower() for x in entries]
 
 
 def send_email(cfg, links):
     email, name, password = cfg["User"]["email"], cfg["User"]["name"], cfg["User"]["Email_Pass"]
+    if email == "" or name == "" or password == "":
+        print("Check that you set all the email options in config.ini!")
+        sys.exit(1)
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     # If using Gmail, generate an app password @ https://myaccount.google.com/apppasswords
     server.login(user=email, password=password)
