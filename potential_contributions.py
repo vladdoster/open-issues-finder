552100ab62ce695f77a298dcae642386999250d7
Vlad Doster <mvdoster@gmail.com>
2020-03-14 19:14:57 -0400

    Added feature to list projects in terminal if no email setup, moved from print to logging, and reworked some code into a list comprehension

--- potential_contributions.py
+++ potential_contributions.py
@@ -1,4 +1,7 @@
 import configparser
+import itertools
+import logging
+import pprint
 import smtplib
 import sys
 from multiprocessing.pool import ThreadPool as Pool
@@ -8,6 +11,8 @@ from time import sleep
 import httpx
 from httpx import get
 
+logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO)
 
 
 def main():
@@ -21,19 +26,15 @@ def main():
 def gen_gh_interests_urls(cfg, interests, labels):
     projects_limit = cfg['User']['projects_limit']
     if projects_limit == "":
-        print("Set a project limit in config")
+        logger.error("Set a project limit in config")
         sys.exit(1)
-    links = []
-    for x in range(int(projects_limit)):
-        interest, label = choice(interests), choice(labels)
-        links.append(
-            "https://api.github.com/search/issues?q={}+label:{}+state:open&sort=created&order=desc".format(interest,
-                                                                                                           label))
+    base_link = "https://api.github.com/search/issues?q={}+label:{}+state:open&sort=created&order=desc"
+    links = [base_link.format(choice(interests), choice(labels)) for _ in itertools.repeat(None, int(projects_limit))]
     return links
 
 
 def multiprocess_links(urls):
-    print("> Finding some projects...")
+    logger.info("> Finding some projects...")
     pool = Pool(10)
     results = [x for x in pool.imap_unordered(get_gh_link, urls) if x is not None]
     return results
@@ -48,9 +49,9 @@ def get_gh_link(url):
         gh_link = choice(r.get('items')).get('html_url')
         return gh_link
     except httpx.exceptions.HTTPError:
-        print("Error: Requesting from Github too often")
+        logger.warning("Requesting from Github too often")
     except Exception as error:
-        print("Something unexpected occurred :{\n{}".format(error))
+        logger.error("Something unexpected occurred: {}".format(error))
 
 
 def get_section(cfg, section):
@@ -58,13 +59,13 @@ def get_section(cfg, section):
         # Normalize interests strings
         entries = cfg.items(section)
         if not entries:
-            print("Add at least one {} in your config.ini".format(section[:-1]))
+            logger.error("Add at least one {} in your config.ini".format(section[:-1]))
             sys.exit(1)
     except configparser.NoSectionError:
-        print("Error: Add a [{}] section to your config.ini".format(section))
+        logger.error("Add a [{}] section to your config.ini".format(section))
         sys.exit(1)
     except OSError:
-        print("Error: No config.ini found.")
+        logger.error("No config.ini found.")
         sys.exit(1)
     return [str(x[0]).lower() for x in entries]
 
@@ -72,7 +73,9 @@ def get_section(cfg, section):
 def send_email(cfg, links):
     email, name, password = cfg["User"]["email"], cfg["User"]["name"], cfg["User"]["Email_Pass"]
     if email == "" or name == "" or password == "":
-        print("Check that you set all the email options in config.ini!")
+        logger.error("Check that you set all the email options in config.ini!")
+        pp = pprint.PrettyPrinter(indent=1)
+        logger.info("Here are the projects we would have emailed you:\n{}".format(pp.pformat(links)))
         sys.exit(1)
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     # If using Gmail, generate an app password @ https://myaccount.google.com/apppasswords
@@ -83,7 +86,7 @@ def send_email(cfg, links):
         to_addrs=email,
         msg=f"""Subject: Potential Contributor\n\nHi {name},\nWe found the following projects you might be interested in contributing too.\n\n{links}""")
     server.quit()
-    print("> Successfully sent Github repos to your email!")
+    logger.info("> Successfully sent Github repos to your email!")
 
 
 if __name__ == '__main__':
