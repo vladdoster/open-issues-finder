06021a7098b2edfa8e1304ec926cff81b1dae3d1
Vlad Doster <mvdoster@gmail.com>
2020-06-30 20:14:36 -0500

    Cleaned script up and added 2 ways to run on system

--- README.md
+++ README.md
@@ -1,28 +1,116 @@
+---
+Author: Vlad Doster <mvdoster@gmail.com>
+Date: 2020-06-30 19:15:59
+Last Modified by: Vlad Doster <mvdoster@gmail.com>
+Last Modified time: 2020-06-30 20:03:06
+---
+
 # Potential Contributor
 
 Having trouble finding an OS project to contribute too? Have projects sent to you that need help!
 
-This app allows you to plug in your programming interests and issue labels to help you find Open Source projects to contribute to. 
+This app allows you to plug in your programming interests and issue labels to help you find Open Source projects to contribute to.
 
 ![](https://github.com/vladdoster/potential_contributions/blob/master/assets/email_screenshot.png)
 
-### Usage
+## Usage
 
-1. Fill out the config.ini with your interests. The interests and labels have some prepopulated to show you what it expects.
+1. Fill out the [config.ini](cofig.ini) with your interests. The interests and labels have some prepopulated to show you what it expects.
 
-2. Run the script with Python >= 3.6
+2. Requires only >=Python 3.x.x and no external dependencies!
 
+```bash
+python3 potential-contributions-finder
 ```
-python potential_contributions.py
+
+3. Look in your email for a list of potential projects to contribute too!
+
+## Set and forget
+
+1. Make script executable:
+
+```bash
+chmod +x potential-contributions-finder
 ```
 
-3. Look in your email for a list of potential projects to contribute too!         
+2. Copy to desired host location:
+   **Note**: `potential-contributions-finder` and `config.ini` should be in same directory.
+   **Sys-Admins** would say it should be placed in `/usr/bin`, but I prefer `$HOME/.local/bin/`. It really comes down to preference.
+   ```bash
+   cp potential-contributions-finder confing.ini $HOME/.local/vim
+   ```
 
-### Cronjob
-To set as a cronjob, the following is how I have it setup. It runs everyday at 9am.
-```
+### Use a `cronjob` to run script on a schedule.
+
+The following is how have it running on my VPS. It runs everyday at 9am.
+
+```bash
 0 9 * * * /usr/bin/python3.8 /home/vlad/.local/bin/potential_contributions.py >/dev/null 2>&1
 ```
 
-### TODO
-- [x] Make systemd unit file or a cronjob so it runs periodically
+1. [Get up to speed on what cron is](https://wiki.archlinux.org/index.php/Cron)
+2. [crontab guru - online crontab editor](https://crontab.guru/)
+
+### Systemd service
+
+1. Create the `.service` file in `/etc/systemd/system`. I'd name it something like `potential-contributor.service`
+
+```bash
+touch /etc/systemd/system/potential-contributor.{service,timer}
+```
+
+2. Edit the `.service` file
+
+```bash
+vim -O potential-contributor.service potential-contributor.timer
+```
+
+3. The contents would look like this.
+
+##### /etc/systemd/system/potential-contributor.service
+
+```bash
+[Unit]
+Description=Send N \# of projects that align with programming interests
+
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/potential-contributions-finder
+
+[Install]
+WantedBy=multi-user.target
+```
+
+##### potential-contributor.timer
+
+```bash
+[Unit]
+Description=Run potential-contributions-finder every 24 hours
+Requires=potential-contributor.service
+
+[Timer]
+Unit=potential-contributor.service
+OnUnitInactiveSec=24h
+AccuracySec=1s
+
+[Install]
+WantedBy=timers.target
+```
+
+If, for some reason your system is not playing nicely with `ExecStart` + absolute path. You can also
+give `ExecStart` a direct sh command like so
+
+```bash
+ExecStart=/bin/sh -c "python3 /usr/bin/potential-contributions-finder"
+```
+
+4. Exit vim. I'll wait...
+
+5. Enable & start newly created service
+
+```bash
+systemctl enable potential-contributor.service potential-contributor.timer
+
+systemctl start potential-contributor.service potential-contributor.timer
+```
+
--- potential_contributions_finder.py
+++ potential_contributions_finder.py
@@ -1,15 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Author: Vlad Doster <mvdoster@gmail.com>
+# Date: 2020-06-30 20:03:53
+# Last Modified by: Vlad Doster <mvdoster@gmail.com>
+# Last Modified time: 2020-06-30 20:13:24
+
 import configparser
 import itertools
 import logging
+import random
 import smtplib
-import collections
 import sys
+import time
+from collections import namedtuple
 from multiprocessing.pool import ThreadPool as Pool
-from random
-from time
 
 import httpx
-from httpx import get
 
 logging.basicConfig(level=logging.INFO, fmt="[%(levelname)s] %(message)s")
 logger = logging.getLogger()
@@ -17,18 +24,33 @@ logger = logging.getLogger()
 
 def main():
     cfg = configparser.ConfigParser(allow_no_value=True).read("config.ini")
-    interests, labels = get_section(cfg, section='Interests'), get_section(cfg, section='Labels')
-    send_email(cfg, multiprocess_links(gen_gh_interests_urls(cfg=cfg, interests=interests, labels=labels)))
+    interests, labels = (
+        get_section(cfg, section="Interests"),
+        get_section(cfg, section="Labels"),
+    )
+    send_email(
+        cfg,
+        multiprocess_links(
+            gen_gh_interests_urls(cfg=cfg, interests=interests, labels=labels)
+        ),
+    )
 
 
 def gen_gh_interests_urls(cfg, interests, labels):
     gh_api_query_link = "https://api.github.com/search/issues?q={0}+label:{1}+state:open&sort=created&order=desc"
     try:
-        return [base_link.format(random.choice(interests), random.choice(labels)) for _ in itertools.repeat(None, int(cfg['User']['projects_limit']))]
+        return [
+            gh_api_query_link.format(random.choice(interests), random.choice(labels))
+            for _ in itertools.repeat(None, int(cfg["User"]["projects_limit"]))
+        ]
     except ValueError:
-        sys.exit(logger.error("Invalid project limit value set in config.ini. It should be an integer."))
+        sys.exit(
+            logger.error(
+                "Invalid project limit value set in config.ini. It should be an integer."
+            )
+        )
 
-        
+
 def multiprocess_links(urls):
     logger.info("Looking high and low for cool projects...")
     return [x for x in Pool(10).imap_unordered(get_gh_link, urls) if x is not None]
@@ -37,13 +59,17 @@ def multiprocess_links(urls):
 def get_gh_link(url):
     time.sleep(random.randint(1, 2))
     try:
-        r = get(url)
+        r = httpx.get(url)
         r.raise_for_status()
-        return random.choice(r.json().get('items')).get('html_url')
+        return random.choice(r.json().get("items")).get("html_url")
     except httpx.exceptions.HTTPError as e:
         logger.warning("Encountering Github API throttling\n{0}".format(e))
     except Exception as e:
-        logger.error("Uh-oh, {0!r} experienced an unexpected bit flip from a solar flare.{1}".format(sys.argv[0], e))
+        logger.error(
+            "Uh-oh, {0!r} experienced an unexpected bit flip from a solar flare.{1}".format(
+                sys.argv[0], e
+            )
+        )
 
 
 def get_section(cfg, section):
@@ -59,39 +85,58 @@ def get_section(cfg, section):
     finally:
         sys.exit(0)
 
-        
+
 def send_email(cfg, links):
-    EmailCreds = namedtuple('EmailCreds', ['name', 'email', 'passwd'])
-    e_c = EmailCreds(name=cfg["User"]["name"], email=cfg["User"]["email"], passwd=cfg["User"]["email_passwd"])
+    EmailCreds = namedtuple("EmailCreds", ["name", "email", "passwd"])
+    e_c = EmailCreds(
+        name=cfg["User"]["name"],
+        email=cfg["User"]["email"],
+        passwd=cfg["User"]["email_passwd"],
+    )
     if any(credential == "" for credential in list(e_c)):
-        logger.info( \
-        """
+        logger.info(
+            """
         Potential Contributor found the following {0} OS projects you might be interested in contributing too.
-        
+
         Links:
         {1}
-        
-        """.format(len(links), '\n'.join("{0}. {1}".format(idx, link) for idx, link in enumerate(links, 1))
-        sys.exit(logger.warning("One of the email User credentials is empty in config.ini. Please reconcile and re-run. Exiting...")))
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        """.format(
+                len(links),
+                "\n".join(
+                    "{0}. {1}".format(idx, link) for idx, link in enumerate(links, 1)
+                ),
+            )
+        )
+        sys.exit(
+            logger.warning(
+                "One of the Email credentials in config.ini is empty in config.ini. Please reconcile and re-run. Exiting..."
+            )
+        )
+    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
     # If using gmail, generate an app password @ https://myaccount.google.com/apppasswords
-    server.login(user=email, password=password)
+    server.login(user=e_c.email, password=e_c.passwd)
     server.sendmail(
         from_addr=e_c.email,
         to_addrs=e_c.email,
-        msg= \
-        """
+        msg="""
         Subject: Potential Contributor
-        
+
         Hi {0},
         Potential Contributor found the following {1} OS projects you might be interested in contributing too.
-        
+
         Links:
         {2}
-        """.format(e_c.name, len(links), '\n'.join("{0}. {1}".format(idx, link) for idx, link in enumerate(links, 1))
+        """.format(
+            e_c.name,
+            len(links),
+            "\n".join(
+                "{0}. {1}".format(idx, link) for idx, link in enumerate(links, 1)
+            ),
+        ),
+    )
     server.quit()
     sys.exit(logger.info("Successfully sent list of project to {0}".format(e_c.name)))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
